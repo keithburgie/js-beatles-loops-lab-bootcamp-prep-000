f7879b6e6475141dc6a90c4df08ad70d883664a2
Keith Burgie <krburgie@gmail.com>
2019-04-17 07:33:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   let array = [];
   do {
-    
-  } while ();
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array;
 }
